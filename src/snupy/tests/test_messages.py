987abb1836be9d934e870660ebb818af81f6bdf4
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-08-24 21:37:17 +0900

    formatted

--- src/snupy/tests/test_messages.py
+++ src/snupy/tests/test_messages.py
@@ -3,14 +3,7 @@ import pytest
 
 class TestGetResponseText:
     @pytest.mark.parametrize(
-        "message", [
-            ("つらい"),
-            ("辛い"),
-            ("頑張る"),
-            ("がんばる"),
-            ("がむばる"),
-            ("がむぱる"),
-        ]
+        "message", [("つらい"), ("辛い"), ("頑張る"), ("がんばる"), ("がむばる"), ("がむぱる"),]
     )
     def test_negative_pattern(self, message):
         from snupy.messages import get_response_text
@@ -18,14 +11,7 @@ class TestGetResponseText:
         assert get_response_text(message) == "ぱにゃにゃんだー🐼😺"
 
     @pytest.mark.parametrize(
-        "message", [
-            ("褒めて"),
-            ("ほめて"),
-            ('頑張った'),
-            ('がんばった'),
-            ('がむばった'),
-            ('がむぱった'),
-        ]
+        "message", [("褒めて"), ("ほめて"), ("頑張った"), ("がんばった"), ("がむばった"), ("がむぱった"),]
     )
     def test_compliment_pattern(self, message):
         from snupy.messages import get_response_text
@@ -40,7 +26,7 @@ class TestGetResponseText:
     def test_choice(self):
         from snupy.messages import get_response_text
 
-        choices = ['hoge', 'fuga', 'piyo']
+        choices = ["hoge", "fuga", "piyo"]
         actual = get_response_text("choice " + ",".join(choices))
 
         expected = [f"I choose this 👉 {e}" for e in choices]
